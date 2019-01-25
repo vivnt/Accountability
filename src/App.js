baeb746b42a3cdb6cf075879fa5c850c92c3ec82
Vivian Tran <ntran.vivian@gmail.com>
2019-01-25 13:46:26 -0800

    Got Eslint to work

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import Home from "./Home"
-import SignUp from "./SignUp"
-import SignIn from "./SignIn"
-import Profile from "./Profile"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Profile from "./Profile";
 
 class AppRouter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
       <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/signin">Sign in</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign in</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Route path="/" exact component={Home}/>
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/signin" component={SignIn}/>
-        <Route path="/profile" component={Profile}/>
-      </div>
-    </Router>)
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </Router>
+    );
   }
 }
 
