1aed65cbb2df86425afed24950ae3610bd9a1199
Vivian Tran <ntran.vivian@gmail.com>
2019-01-30 10:19:35 -0800

    Added navbar with integration with react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Container, Menu, Button } from "semantic-ui-react";
 import Home from "./Home";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
@@ -14,29 +15,31 @@ class AppRouter extends React.Component {
   render() {
     return (
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="/signin">Sign in</Link>
-              </li>
-              <li>
-                <Link to="/profile">Profile</Link>
-              </li>
-            </ul>
-          </nav>
+        <Container>
+          <Menu fixed="top">
+            <Container>
+              <Menu.Item as={Link} to="/">
+                Home
+              </Menu.Item>
+              <Menu.Item as={Link} to="/profile">
+                Profile
+              </Menu.Item>
+              <Menu.Item position="right">
+                <Button as={Link} to="/signin">
+                  Sign In
+                </Button>
+                <Button as={Link} to="/signup" style={{ marginLeft: "15px" }}>
+                  Sign Up
+                </Button>
+              </Menu.Item>
+            </Container>
+          </Menu>
 
           <Route path="/" exact component={Home} />
           <Route path="/signup" component={SignUp} />
           <Route path="/signin" component={SignIn} />
           <Route path="/profile" component={Profile} />
-        </div>
+        </Container>
       </Router>
     );
   }
